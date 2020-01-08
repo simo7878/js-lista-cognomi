51a6c8e490dbe1c78647c5a0b5b7c76f55ada42f
simona <palumbosimona118@gmail.com>
2020-01-08 19:33:15 +0100

    ex cognomi

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//Chiedi il proprio cognome all'utente
+var cognomeUtente = prompt('inserisci il tuo cognome');
+console.log(cognomeUtente);
+
+//inserire il cognome in un array con altri cognomi
+var cognomi = ['Sabellico', 'Palumbo', 'Gioia', 'Landi', 'Rossi', 'Venditti', 'Corelli'];
+cognomi.push(cognomeUtente);
+console.log(cognomi);
