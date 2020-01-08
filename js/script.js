ddccc8d53d44ba4401d708a51b48b95f5b203162
simona <palumbosimona118@gmail.com>
2020-01-08 20:02:59 +0100

    commit finale

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+alert('welcome');
 //Chiedi il proprio cognome all'utente
 var cognomeUtente = prompt('inserisci il tuo cognome');
 console.log(cognomeUtente);
@@ -6,3 +7,16 @@ console.log(cognomeUtente);
 var cognomi = ['Sabellico', 'Palumbo', 'Gioia', 'Landi', 'Rossi', 'Venditti', 'Corelli'];
 cognomi.push(cognomeUtente);
 console.log(cognomi);
+
+//pushare il cognome nella lista
+cognomi.push(cognomeUtente);
+
+// ordinare i cognomi in ordine alfabetico
+cognomi.sort();
+for (var i=0; i<cognomi.length; i++){
+  document.getElementById('lista_cognomi').innerHTML += '<li>'+ cognomi[i] + '</li>';
+  }
+
+//specificare la posizione del cognome utente nella lista
+  var position = cognomi.indexOf(cognomeUtente) + 1;
+  document.getElementById("position").innerHTML = "posizione numero " + position + " dell'elenco utenti.";
